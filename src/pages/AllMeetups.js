67018bcccb4c5bd1515b80527c76ea64075a40ab
Roshan Joseph <roshanjosephse01@gmail.com>
2022-12-13 23:09:53 +0530

    first commit

---
+++ src/pages/AllMeetups.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+
+function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [LoadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://react-project-f2e0d-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups=[];
+
+        for(const key in data){
+          const  meetup={
+            id:key,
+            ...data[key]
+          };
+
+          meetups.push(meetup);
+        }
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={LoadedMeetups} />
+    </section>
+  );
+}
+
+export default AllMeetupsPage;
